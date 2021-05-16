bbddf45fe6805e3768daab943a513bd8e1661187
kghugo <hugosum.dev@protonmail.com>
2021-05-16 17:32:38 +0800

    Use v to alias vim.api

--- lua/range-highlight/init.lua
+++ lua/range-highlight/init.lua
@@ -1,8 +1,9 @@
-local ns = vim.api.nvim_create_namespace('rang-highlight')
+local v = vim.api
+local ns = v.nvim_create_namespace('rang-highlight')
 local opts, cache = {highlight = "Visual"}, {}
 
 local function cleanup()
-    vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
+    v.nvim_buf_clear_namespace(0, ns, 0, -1)
     cache = {}
 end
 
@@ -11,8 +12,8 @@ local function add_highlight()
     local index = 1
     local start_line, end_line = nil, nil
     local arr = {}
-    for v in string.gmatch(text, "%d+") do
-        arr[index] = v
+    for value in string.gmatch(text, "%d+") do
+        arr[index] = value
         index = index + 1
         if index > 2 then break end
     end
@@ -26,7 +27,7 @@ local function add_highlight()
     -- if cache[1] == start_line and cache[2] == end_line then return end
     if cache[1] and cache[2] then
         if cache[1] ~= start_line or cache[2] ~= end_line then
-            vim.api.nvim_buf_clear_namespace(0, ns, cache[1], cache[2])
+            v.nvim_buf_clear_namespace(0, ns, cache[1], cache[2])
         end
     end
     if end_line < start_line then return end
@@ -38,7 +39,7 @@ end
 
 local function setup(user_opts)
     opts = vim.tbl_extend('force', opts, user_opts or {})
-    vim.api.nvim_exec([[ 
+    v.nvim_exec([[ 
 	augroup Ranger
 	autocmd!
 	au CmdlineChanged * lua require('range-highlight').add_highlight()
